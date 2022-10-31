965e060c644d321a95d8ccc531ae7360f971bc39
Erwin Errol Calanuga <hortsatta@gmail.com>
2022-10-31 10:49:13 +0800

    Add toast notification

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,38 @@
 import { NextUIProvider } from '@nextui-org/react';
+import { ToastContainer } from 'react-toastify';
 import type { AppProps } from 'next/app';
 
 import { globalStyles, theme } from '@/utils/theme.util';
 import { CoreLayout } from '@/components/core';
 
 import 'simplebar/dist/simplebar.min.css';
+import 'react-toastify/dist/ReactToastify.css';
 import '@/fonts/typography.css';
 
 const App = ({ Component, pageProps }: AppProps) => {
   globalStyles();
-
   return (
-    <NextUIProvider theme={theme}>
-      <CoreLayout>
-        <Component {...pageProps} />
-      </CoreLayout>
-    </NextUIProvider>
+    <>
+      <NextUIProvider theme={theme}>
+        <CoreLayout>
+          <Component {...pageProps} />
+        </CoreLayout>
+      </NextUIProvider>
+      <ToastContainer
+        theme='dark'
+        position='bottom-center'
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        rtl={false}
+        limit={2}
+        icon={false}
+        pauseOnFocusLoss
+        closeOnClick
+        draggable
+        pauseOnHover
+      />
+    </>
   );
 };
 
