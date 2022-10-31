087135303701bdf2f2624b08b1b9ac86096397d6
Erwin Errol Calanuga <hortsatta@gmail.com>
2022-10-31 10:49:36 +0800

    Fix graphql server

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,18 +1,23 @@
 import { createServer } from '@graphql-yoga/node';
 import { typeDefs } from '@/graphql/type-defs';
 import { resolvers } from '@/graphql/resolvers';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
 import { supabase } from '@/utils/supabase-client.util';
 
-const server = createServer({
+export const config = {
+  api: { bodyParser: false },
+};
+
+export default createServer<{
+  req: NextApiRequest;
+  res: NextApiResponse;
+}>({
   schema: {
     typeDefs,
     resolvers,
   },
-  context: () => ({
-    supabase,
-  }),
+  context: (ctx) => ({ ...ctx, supabase }),
   endpoint: '/api/graphql',
   graphiql: process.env.NODE_ENV !== 'production',
 });
-
-export default server;
