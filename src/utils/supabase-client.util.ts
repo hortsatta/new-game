2c69c1043c42202b7055af40e16759bdad30d228
Erwin Errol Calanuga <hortsatta@gmail.com>
2022-10-31 10:50:35 +0800

    Update supabase client creation

--- src/utils/supabase-client.util.ts
+++ src/utils/supabase-client.util.ts
@@ -3,10 +3,14 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.SUPABASE_API_URL || '';
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY || '';
 const options = {
-  schema: 'public',
-  autoRefreshToken: true,
-  persistSession: true,
-  detectSessionInUrl: true,
+  db: {
+    schema: 'public',
+  },
+  auth: {
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: true,
+  },
 };
 
 export const supabase = createClient(supabaseUrl, supabaseKey, options);
