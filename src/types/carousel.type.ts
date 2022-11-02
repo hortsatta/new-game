649e560a9ad8f1f8317f814ec22c514760952128
Erwin Errol Calanuga <hortsatta@gmail.com>
2022-11-02 20:47:40 +0800

    Add carousel type

--- src/types/carousel.type.ts
+++ src/types/carousel.type.ts
@@ -1,15 +1,35 @@
-export enum CarouselType {
-  GAME,
-  IMAGE,
-}
+import { GameProduct } from './game-product.type';
 
-export type Image = {
+type CarouseImage = {
   url: string;
   name?: string;
 };
 
-export type CarouselItem = {
-  type: CarouselType;
-  content: any | Image;
+type CarouselGameProduct = GameProduct & {
+  imageUrl: string;
+};
+
+type CarouselImageContent = {
+  type: 'image';
+  image: CarouseImage;
+  excerpt?: string;
+};
+
+type CarouselGameProductContent = {
+  type: 'game';
+  gameProduct: CarouselGameProduct;
+  excerpt?: string;
+};
+
+type CarouselItem = {
+  content: CarouselImageContent | CarouselGameProductContent;
   index?: number;
+};
+
+export type {
+  CarouseImage,
+  CarouselGameProduct,
+  CarouselImageContent,
+  CarouselGameProductContent,
+  CarouselItem,
 };
