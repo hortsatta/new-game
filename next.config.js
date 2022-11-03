6fb940b756b5603aa4e31ae863c1b10ef3956524
Erwin Errol Calanuga <hortsatta@gmail.com>
2022-11-03 14:05:33 +0800

    Add image hostname

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'media.rawg.io',
+        pathname: '/media/games/**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
